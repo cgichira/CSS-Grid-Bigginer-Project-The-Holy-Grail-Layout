459e154c8d29d3c9e7e13f2c7ba5c4222198e66d
cgichira <107805102+cgichira@users.noreply.github.com>
2023-04-14 21:38:16 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+"# CSS-Grid-Bigginer-Project-The-Holy-Grail-Layout" 
---
+++ script.js
@@ -0,0 +1,6 @@
+const hideNav = document.querySelector(".outersection")
+const turnNavDisplayOff = document.querySelector(".outer-nav")
+function toggleclass(){
+    hideNav.classList.toggle("navpanel-removed");
+    turnNavDisplayOff.classList.toggle("navpanel-display-off")
+}
